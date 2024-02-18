283dfb71270ccd8c621314c17518b95a796aadd4
arl20 <150931058+arl20@users.noreply.github.com>
2024-02-18 17:05:17 +0300

    Delete aiogram_tests directory

--- aiogram_tests/handler/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import RequestHandler
-from .handler import (CallbackQueryHandler, MessageHandler,
-                      TelegramEventObserverHandler)
-
-__all__ = ["MessageHandler", "CallbackQueryHandler", "TelegramEventObserverHandler", "RequestHandler"]
