d6ebe84cf12ccc5b307a757dfe15c5e4b2e82523
root <kubaevaassol@yandex.ru>
2024-02-25 20:59:23 +0300

    Minor changes

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import gdown
 from aiogram import Bot, Dispatcher
 
 from handlers import commands, inline_router, reviews
-from utils.functions_for_recommendation import load_data
+from utils.utils import load_data
 
 
 async def main():
@@ -20,22 +20,22 @@ async def main():
     K_GAMES = config.getint('bot', 'K_GAMES')
     URL = config.get('bot', 'URL')
     logging.basicConfig(filename='log.txt', level=logging.INFO)
-    user_data = dict()
+    logging.log(msg="Load data", level=logging.INFO)
     try:
         df, matrix, app_id_to_index, user_id_to_index, similar_games_df = await asyncio.to_thread(load_data)
     except FileNotFoundError:
         gdown.download_folder(URL, remaining_ok=True)
         df, matrix, app_id_to_index, user_id_to_index, similar_games_df = await asyncio.to_thread(load_data)
-    df = df.sort_values(by='TotalReviews')
     games_info_dict_by_name = df[~df.duplicated(subset=['Name'],
                                                 keep='last')].set_index('Name')[['AppID']].to_dict(orient='index')
     games_info_dict = df.set_index('AppID')[['Name', 'Price',
                                              'Required age', 'About the game',
                                              'Supported languages', 'Genres']].to_dict(orient='index')
+    logging.log(msg="Bot started", level=logging.INFO)
     bot = Bot(token=token)
     dp['K_NEIGHBOURS'] = K_NEIGHBOURS
     dp['K_GAMES'] = K_GAMES
-    await dp.start_polling(bot, df=df, user_data=user_data,
+    await dp.start_polling(bot, df=df,
                            games_info_dict=games_info_dict,
                            games_info_dict_by_name=games_info_dict_by_name,
                            similar_games_df=similar_games_df,
